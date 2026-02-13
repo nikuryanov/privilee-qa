50d845d3742832a86a70bf246351df39cf54152c
nkur <nkuryanovv@gmail.com>
2026-02-13 14:35:56 +0300

    Add authorization header to API tests using env token

--- tests/api/test_posts.py
+++ tests/api/test_posts.py
@@ -1,14 +1,12 @@
-import requests
 import os
+import requests
 
 BASE_URL = "https://gorest.co.in/public/v2"
 
+# Get your GoRest personal access token from environment variables
 API_TOKEN = os.getenv("GOREST_TOKEN")
+HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}  # Used in all requests
 
-HEADERS = {
-    "Authorization": f"Bearer {API_TOKEN}"
-}
-    
 def test_get_posts_status_code():
     response = requests.get(f"{BASE_URL}/posts", headers=HEADERS)
     assert response.status_code == 200
